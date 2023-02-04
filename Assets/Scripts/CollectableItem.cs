8c52fd2c1387b04eeb4a93a5cc1b8dacb40ba847
MatiM36 <38538737+MatiM36@users.noreply.github.com>
2023-02-04 19:52:35 -0300

    Small Collectible
    Added small collectible
    Fixed collectibles not resetting when losing

--- Assets/Scripts/CollectableItem.cs
+++ Assets/Scripts/CollectableItem.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollectableItem : MonoBehaviour
+public class CollectableItem : MonoBehaviour, IReseteable
 {
     public Hurtbox hurtbox;
     public ParticleSystem pickParticle;
@@ -13,10 +13,25 @@ public class CollectableItem : MonoBehaviour
         hurtbox.OnDamageReceived += OnPlayerCollected;
     }
 
+    private void Start()
+    {
+        LevelManager.Instance.RegisterLevelObject(this);
+    }
+
+    private void OnDestroy()
+    {
+        LevelManager.Instance.UnregisterLevelObject(this);
+    }
+
+    public void ResetObject()
+    {
+        gameObject.SetActive(true);
+    }
+
     private void OnPlayerCollected()
     {
         if(pickParticle != null)
             Instantiate(pickParticle, transform.position, Quaternion.identity, null);
-        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
 }
